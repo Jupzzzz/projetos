3cc1eadb0d7809822cf8c666902b275fa04cfff6
Júnior Gomes <206622250+Jupzzzz@users.noreply.github.com>
2026-04-27 12:43:53 -0300

    sorting algorithm funcionando, mas ainda falta verificar se o sorting acabou

--- sort_visualizer.c
+++ sort_visualizer.c
@@ -1,14 +1,11 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
-
-#define WIDTH 900
-#define HEIGHT 600
-
-#define COUNT 10
+#define COUNT 50
 
 int numbers[COUNT];
 
@@ -17,25 +14,68 @@ void draw_bars()
     for ( int i = 0; i < COUNT; i++)
     {
         int value = numbers[i];
-        DrawRectangle(i*50,HEIGHT*0.75,20,value*HEIGHT*0.75/COUNT,WHITE);
+        int bar_height = value*HEIGHT*0.75/COUNT;
+        DrawRectangle(((float)i/COUNT)*WIDTH,HEIGHT*0.75 - bar_height,WIDTH/COUNT-2,bar_height,WHITE);
     }
     
 }
 
-int main()
+void swap(int i,int j)
+{
+    int tmp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = tmp;
+}
+void init_numbers()
 {
     for (int i = 0; i < COUNT; i++)
     {
         numbers[i]=i;
-        printf("numbers[i] = %d\n", numbers[i]);
-    }    
+    }
+    
+    //Fisher Yates Shuffer
+    for (int i = COUNT-1; i >= 0; i--)
+    {
+        //generates a random number between 0 and i
+        int j  = rand() % (i + 1); 
+
+        //swap the arrays entries
+        swap(i,j);
+    }
+}
+
+void sort_step()
+{
+    static int i = 0;
+    int current_value = numbers[i];
+    int next_value = numbers[i+1];
+    
+    if (i >= COUNT-1)
+    {
+        i=0;
+        return;
+    }
+    if (current_value > next_value)
+        {
+            swap(i,i+1);
+        }
+    
+    i++;
+}
+
+int main()
+{
+    init_numbers();   
 
     InitWindow(WIDTH, HEIGHT, "Sort Visual");
 
+    SetTargetFPS(COUNT);
     while (!WindowShouldClose())
     {
+        sort_step();
+
         BeginDrawing();
-        
+        ClearBackground(BLACK);
         //desenhar as barras
         draw_bars();
 
